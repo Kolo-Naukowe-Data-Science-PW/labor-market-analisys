fe453cca79de226396c186c460709da0ffed65db
rifti <michal.kurtys@gmail.com>
2015-03-06 15:50:31 +0100

    First try
    gall.py - script that downloads list of job offers from pracuj.pl
    dd_offer.py - script that extracts offer content. Then it is converted
    from html to pure text. I am still thinking if it is really good idea to
    throw away all html tags.
    I am strill thinking about representation of lists. Now I write them in
    pseudo-markdown form.
    
    I know that the Mess is overwhelming.

---
+++ dd_offer.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+import urllib2
+import lxml.html
+import sys
+import unicodecsv
+
+
+#This script downloads work offers from pracuj.pl portal
+#WARNING: number of work offers pages is hardcoded in num_pages variable
+#TODO: check for timeout errors
+#TODO: add some sleep time in order not to flood servers
+
+
+def extract_description(page):
+	response = urllib2.urlopen(page)
+	html = response.read()
+
+	t = lxml.html.fromstring(html)
+	t.make_links_absolute(page)
+	
+	offer = t.xpath("//div[@id='offCont']")
+	#print(type(offer[0]))
+	
+	
+	
+	if( len(offer) > 0 ):
+		#change <br> to newline
+		for br in offer[0].xpath("*//br"):
+			br.tail = "\n" + br.tail if br.tail else "\n"
+		for br in offer[0].xpath("*//p"):
+			br.tail = "\n" + br.tail if br.tail else "\n"
+		for br in offer[0].xpath("*//li"):
+			br.tail = "\n" + br.tail if br.tail else "\n"
+			
+		for li in offer[0].xpath("*//li"):
+			if li.text:
+				li.text = "*" + li.text
+		#offer_html = lxml.html.tostring(offer[0])
+		#return offer_html
+		offer_text = lxml.html.HtmlElement.text_content(offer[0])
+		return offer_text
+	else:
+		return ""
+
+h = extract_description("http://www.pracuj.pl/praca/\
+inzynier-procesu-przetworstwo-tworzyw-sztucznych-lodzkie,oferta,3811165")
+print(h.encode('ascii', 'replace'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ gall.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+import urllib2
+import lxml.html
+import sys
+import unicodecsv
+
+
+#This script downloads work offers from pracuj.pl portal
+#WARNING: number of work offers pages is hardcoded in num_pages variable
+#TODO: check for timeout errors
+#TODO: add some sleep time in order not to flood servers
+
+def parse_page(page):
+	response = urllib2.urlopen(page)
+	html = response.read()
+
+	t = lxml.html.fromstring(html)
+	t.make_links_absolute(page)
+	entries = t.xpath("//ul[@id='mainOfferList']/li")
+	
+	offers = []
+	for entry in entries:
+		position = entry.xpath("h2/a/@title")
+		position_url = entry.xpath("h2/a/@href")
+		
+		#niektóre og³osznia nie maj¹ w ogóle w³asnej podstrony
+		#wtedy oczywiœcie nie ma do niej linku
+		if( len(position) == 0 ):
+			position = entry.xpath("/h2/span/text()")
+			position_url = [""]
+			
+			#w tym przypadku nie jest to w ogóle og³oszenie
+			#a element typu "znajdŸ podobne oferty"
+			if(len(position) == 0):
+				continue
+		
+		company_name = entry.xpath("//h3/a/@title")
+		company_url = entry.xpath("h3/a/@href")
+		
+		offers.append( [position[0],position_url[0],company_name[0],company_url[0]] )
+	
+	return offers
+
+
+
+page_pattern = "http://www.pracuj.pl/praca?pn="
+#num_pages = 396
+num_pages = 2
+
+results = []
+for p in range(1,num_pages+1):
+	url = page_pattern+str(p)
+	print("Parsing page {0}".format(url))
+	page_results = parse_page(url)
+	results.extend(page_results)
+	
+print(results)
+
+#in Python 2 orginal python module csv can't handle unicode data
+#with open("results.csv", "w") as f:
+#	writer = csv.writer(f)
+#	writer.writerows(results)
+
+
+with open("results.csv", "w") as f:
+	writer = unicodecsv.writer(f, encoding='utf-8',delimiter='\t',quotechar="\"", quoting=unicodecsv.QUOTE_ALL)
+	writer.writerows(results)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
